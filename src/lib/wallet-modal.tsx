2844a1249e8d76b9d86c98155b078bca56259ce0
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2024-11-26 22:59:05 +0530

    testing

---
+++ src/lib/wallet-modal.tsx
@@ -0,0 +1,10 @@
+ 
+"use client";
+ 
+import { useWeb3Modal } from "@web3modal/wagmi/react";
+ 
+export default function ConnectButton() {
+  const { open } = useWeb3Modal();
+ 
+  return <button onClick={() => open()}>Connect Wallet</button>;
+}
